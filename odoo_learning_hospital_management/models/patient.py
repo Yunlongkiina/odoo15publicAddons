d9a798bf56a88b89f279aef9b5932f2604e7e1da
Yunlongkiina <yunlong.a.liu@gmail.com>
2023-01-12 10:47:33 +0200

    Add  chatter feature

--- odoo_learning_hospital_management/models/patient.py
+++ odoo_learning_hospital_management/models/patient.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api
 
 class HospitalPaitent(models.Model):
     _name = 'hospital.patient'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Patient'
 
     name = fields.Char()
