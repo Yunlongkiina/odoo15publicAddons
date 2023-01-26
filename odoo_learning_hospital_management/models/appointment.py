965a2f7688aefc73186e072f40c7830979dffb5f
Yunlongkiina <yunlong.a.liu@gmail.com>
2023-01-26 11:49:00 +0200

    Add group restict for developer

--- odoo_learning_hospital_management/models/appointment.py
+++ odoo_learning_hospital_management/models/appointment.py
@@ -32,8 +32,14 @@ class HospitalAppointment(models.Model):
     ], string="Status", default="draft", required=True)
 
     doctor_id = fields.Many2one('res.users', string="Doctor")
+    # pharmacy_line_ids will create many lines,
+    # same idea as sales order line
+    # The model hospital.pharmacy.line need to declare
+    # corresponding Many2one field
     pharmacy_line_ids = fields.One2many(
         'hospital.pharmacy.line', 'appointment_id', string="Pharmacy Line")
+
+    hide_sale_price = fields.Boolean(string="Hide Sale Price")
 
     @api.onchange('patient_id')
     def onchange_patient_ref(self):
@@ -43,7 +49,7 @@ class HospitalAppointment(models.Model):
         return {
             'effect': {
                 'fadeout': 'slow',
-                'message': 'Oh yeah!!!!!!!!!!!!!!!!!!',
+                'message': 'Congraduations! Hope you recover soon!',
                 'type': 'rainbow_man'
             }
         }
@@ -53,6 +59,13 @@ class HospitalAppointment(models.Model):
 
     def action_done(self):
         self.state = 'done'
+        return {
+            'effect': {
+                'fadeout': 'slow',
+                'message': 'Oh yeah!!!!!!!!!!!!!!!!!!',
+                'type': 'rainbow_man'
+            }
+        }
 
     def action_cencel(self):
         self.state = 'cancel'
@@ -63,7 +76,8 @@ class AppointmentPharmacyLines(models.Model):
     _description = 'Appointment Pharmacy Lines'
 
     product_id = fields.Many2one('product.product')
-    price_unit = fields.Float(string="price")
+    price_unit = fields.Float(string="Sale price")
     quantity = fields.Integer(string="Quantity")
+    #
     appointment_id = fields.Many2one(
         'hospital.appointment', string="Appointment")
